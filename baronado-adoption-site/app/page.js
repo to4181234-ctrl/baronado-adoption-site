b4aad13ef9c3cc5910722078deba285a67ec0d84
to4181234-ctrl <to4181234@gmail.com>
2026-06-22 17:48:54 +0900

    Delete baronado-adoption-site directory

--- baronado-adoption-site/app/page.js
+++
@@ -1,6 +0,0 @@
-import DogListPage from '../components/DogListPage';
-import { CATEGORY_ADOPTION } from '../lib/dogs';
-
-export default function HomePage() {
-  return <DogListPage title="가족을 찾아요" category={CATEGORY_ADOPTION} />;
-}
--- baronado-adoption-site/app/residents/page.js
+++
@@ -1,6 +0,0 @@
-import DogListPage from '../../components/DogListPage';
-import { CATEGORY_RESIDENT } from '../../lib/dogs';
-
-export default function ResidentsPage() {
-  return <DogListPage title="상주견 소개" category={CATEGORY_RESIDENT} />;
-}
